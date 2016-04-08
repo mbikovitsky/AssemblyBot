c91415aa3e8e54093713f1205e317752e876317e
Michael Bikovitsky <moshe.biko@gmail.com>
2016-04-08 20:35:33 +0300

    Switch to asyncio

--- assembly_bot.py
+++ assembly_bot.py
@@ -2,9 +2,10 @@
 
 
 import telepot
+import telepot.async
 
 
-class AssemblyBot(telepot.Bot):
+class AssemblyBot(telepot.async.Bot):
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         print('Normal Message:', content_type, chat_type, chat_id)
--- server.py
+++ server.py
@@ -2,17 +2,49 @@
 
 
 import os
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from app import app
+import asyncio
+from urllib.parse import urlunparse
+from aiohttp import web
+from assembly_bot import AssemblyBot
 
 
-def main():
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(int(os.environ["PORT"]))
-    IOLoop.instance().start()
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+SERVER_NAME = os.environ["SERVER_NAME"]
+PORT = int(os.environ["PORT"])
+HOST = "0.0.0.0"
+WEBHOOK_ROUTE = "/" + TELEGRAM_TOKEN
 
 
-if __name__ == "__main__":
-    main()
+class WebhookView(web.View):
+    """Handles requests to the webhook URL"""
+
+    async def get(self):
+        return await self.post()
+
+    async def post(self):
+        data = await self.request.text()
+        await self.request.app["update_queue"].put(data)
+        return web.Response(body="OK".encode("UTF-8"))
+
+
+async def init(loop):
+    # Initialize the server
+    app = web.Application(loop=loop)
+    app["update_queue"] = asyncio.Queue(loop=loop)
+    app.router.add_route("*", WEBHOOK_ROUTE, WebhookView)
+    server = await loop.create_server(app.make_handler(), HOST, PORT)
+
+    # Initialize the bot
+    bot = AssemblyBot(TELEGRAM_TOKEN, loop=loop)
+    loop.create_task(bot.messageLoop(source=app["update_queue"]))
+    await bot.setWebhook(urlunparse(("https",
+                                     SERVER_NAME,
+                                     WEBHOOK_ROUTE,
+                                     "", "", "")))
+
+    return server
+
+
+loop = asyncio.get_event_loop()
+server = loop.run_until_complete(init(loop))
+loop.run_forever()
