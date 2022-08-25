a2e26b999011d301ebe78eec08f33e73217ba2e8
Michael Bikovitsky <biko@bikodbg.com>
2022-08-25 23:35:20 +0300

    Clean up code

--- assembly_bot.py
+++ assembly_bot.py
@@ -1,34 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
 
+
+import binascii
+import html
+import random
+import re
 
 import telepot
 import telepot.aio
-import re
-import binascii
-import random
-import cgi
-from capstone import *
-from keystone import *
+from capstone import CS_ARCH_X86, CS_MODE_32, CS_MODE_64, Cs
+from keystone import KS_ARCH_X86, KS_MODE_32, KS_MODE_64, Ks
 
 
 class BotException(Exception):
     """Base class for exceptions raised by the bot"""
-    pass
 
 
 class AssemblyBot(telepot.aio.Bot):
-    UNRECOGNIZED_CONTENT_TYPES = ("voice", "sticker", "photo", "audio",
-                                  "document", "video", "contact", "location")
+    _UNRECOGNIZED_CONTENT_TYPES = (
+        "voice",
+        "sticker",
+        "photo",
+        "audio",
+        "document",
+        "video",
+        "contact",
+        "location",
+    )
 
-    COMMAND_REGEX = re.compile(r"^\s*/(?P<command>help|about).*$",
-                               re.DOTALL | re.IGNORECASE)
+    _COMMAND_REGEX = re.compile(
+        r"^\s*/(?P<command>help|about).*$", re.DOTALL | re.IGNORECASE
+    )
 
-    MESSAGE_REGEX = re.compile(r"^(?:\s*\(\s*(?P<arch>x86|x64)\s*\)\s*)?"
-                               r"(?:(?P<bytes>(?:[0-9a-f]{2})+)|"
-                               r"(?P<assembly>\S.*))$",
-                               re.DOTALL | re.IGNORECASE)
+    _MESSAGE_REGEX = re.compile(
+        r"^(?:\s*\(\s*(?P<arch>x86|x64)\s*\)\s*)?"
+        r"(?:(?P<bytes>(?:[0-9a-f]{2})+)|"
+        r"(?P<assembly>\S.*))$",
+        re.DOTALL | re.IGNORECASE,
+    )
 
-    USAGE_TEXT = """I can assemble and disassemble various instructions.
+    _USAGE_TEXT = """I can assemble and disassemble various instructions.
 
 To assemble, send me a message in the following format:
 <pre>  (arch) instruction1; instruction2</pre>
@@ -56,10 +68,10 @@ Currently, the supported architectures are:
 If the architecture is omitted, x86 is assumed.
 """
 
-    ABOUT_TEXT = "I'm AssemblyBot. My code is available on " +\
-                 "<a href=\"https://github.com/mbikovitsky/AssemblyBot\">" +\
-                 "GitHub" +\
-                 "</a>."
+    _ABOUT_TEXT = (
+        "I'm AssemblyBot. My code is available on "
+        '<a href="https://github.com/mbikovitsky/AssemblyBot">GitHub</a>.'
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -67,9 +79,9 @@ If the architecture is omitted, x86 is assumed.
 
     async def on_chat_message(self, message):
         try:
-            content_type, chat_type, chat_id = telepot.glance(message)
+            content_type = telepot.glance(message)[0]
 
-            if content_type in self.UNRECOGNIZED_CONTENT_TYPES:
+            if content_type in self._UNRECOGNIZED_CONTENT_TYPES:
                 raise BotException("Message content not understood.")
 
             message_text = message["text"]
@@ -79,19 +91,11 @@ If the architecture is omitted, x86 is assumed.
             else:
                 result = self._process_query_text(message["text"])
             await self._send_reply(message, result)
-        except Exception as e:
-            try:
-                exception_string = str(e)
-            except:
-                exception_string = "Unprintable exception."
-            finally:
-                error_message = "ERROR: " + exception_string
-                await self._send_reply(message,
-                                       self._format_as_html(error_message))
+        except Exception as exception:  # pylint: disable=broad-except
+            await self._send_reply(message, self._format_as_html(f"ERROR: {exception}"))
 
     async def on_inline_query(self, message):
-        query_id, from_id, query_string = telepot.glance(message,
-                                                         flavor="inline_query")
+        query_string = telepot.glance(message, flavor="inline_query")[2]
 
         def _compute_answer():
             result = self._process_query_text(query_string)
@@ -103,60 +107,60 @@ If the architecture is omitted, x86 is assumed.
                     "title": "0xDEADBEEF",
                     "input_message_content": {
                         "message_text": result,
-                        "parse_mode": "HTML"
-                    }
+                        "parse_mode": "HTML",
+                    },
                 }
             ]
 
         self._answerer.answer(message, _compute_answer)
 
     async def _send_reply(self, message, reply):
-        await self.sendMessage(message["chat"]["id"],
-                               reply,
-                               parse_mode="HTML")
+        await self.sendMessage(message["chat"]["id"], reply, parse_mode="HTML")
 
     @staticmethod
     def _format_as_html(text):
-        return "<pre>{}</pre>".format(cgi.escape(text))
+        return f"<pre>{html.escape(text)}</pre>"
 
     @staticmethod
     def _generate_random_id():
         # Return a random ID of at most 64 chars in length
         return hex(random.randint(0, 2 ** (32 * 8) - 1))[2:]
 
-    def _is_command(self, text):
-        return bool(self.COMMAND_REGEX.fullmatch(text))
+    @classmethod
+    def _is_command(cls, text):
+        return bool(cls._COMMAND_REGEX.fullmatch(text))
 
-    def _process_command_text(self, text):
-        match = self.COMMAND_REGEX.fullmatch(text)
+    @classmethod
+    def _process_command_text(cls, text):
+        match = cls._COMMAND_REGEX.fullmatch(text)
         if not match:
             raise BotException("Unrecognized command.")
 
         command = match.group("command").lower()
         if command == "help":
-            return self.USAGE_TEXT
+            return cls._USAGE_TEXT
         elif command == "about":
-            return self.ABOUT_TEXT
+            return cls._ABOUT_TEXT
         else:
             raise BotException("Unrecognized command.")
 
-    def _process_query_text(self, text):
-        match = self.MESSAGE_REGEX.fullmatch(text)
+    @classmethod
+    def _process_query_text(cls, text):
+        match = cls._MESSAGE_REGEX.fullmatch(text)
         if not match:
             raise BotException("Syntax error.")
 
         if match.group("bytes"):
-            result = self._process_bytes(match.group("arch"),
-                                         match.group("bytes"))
+            result = cls._process_bytes(match.group("arch"), match.group("bytes"))
         elif match.group("assembly"):
-            result = self._process_assembly(match.group("arch"),
-                                            match.group("assembly"))
+            result = cls._process_assembly(match.group("arch"), match.group("assembly"))
         else:
             raise BotException("Not supported.")
 
-        return self._format_as_html(result)
+        return cls._format_as_html(result)
 
-    def _process_bytes(self, architecture, raw_bytes):
+    @staticmethod
+    def _process_bytes(architecture, raw_bytes):
         architecture = architecture.lower() if architecture else "x86"
         if architecture == "x86":
             disassembler = Cs(CS_ARCH_X86, CS_MODE_32)
@@ -167,11 +171,13 @@ If the architecture is omitted, x86 is assumed.
 
         binary = binascii.unhexlify(raw_bytes)
 
-        return "\n".join("0x%x:\t%s\t%s" % (address, mnemonic, op_str)
-                         for address, size, mnemonic, op_str
-                         in disassembler.disasm_lite(binary, 0))
+        return "\n".join(
+            f"0x{address:x}:\t{mnemonic}\t{op_str}"
+            for address, _size, mnemonic, op_str in disassembler.disasm_lite(binary, 0)
+        )
 
-    def _process_assembly(self, architecture, text):
+    @staticmethod
+    def _process_assembly(architecture, text):
         architecture = architecture.lower() if architecture else "x86"
         if architecture == "x86":
             assembler = Ks(KS_ARCH_X86, KS_MODE_32)
--- server.py
+++ server.py
@@ -1,24 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
 
 
-import os
 import asyncio
-from aiohttp import web
-from assembly_bot import AssemblyBot
+import os
 from contextlib import closing
+
+from aiohttp import web
+
+from assembly_bot import AssemblyBot
 
 
 g_update_queue = None
 
 
-async def webhook(request):
+async def _webhook(request):
     data = await request.text()
     await g_update_queue.put(data)
     return web.Response(body="OK".encode("UTF-8"))
 
 
-def main():
-    global g_update_queue
+def _main():
+    global g_update_queue  # pylint: disable=global-statement
 
     with closing(asyncio.get_event_loop()) as loop:
         token = os.environ["TELEGRAM_TOKEN"]
@@ -27,10 +30,7 @@ def main():
         bot = AssemblyBot(token)
 
         # Initialize webhooks if we have a URL
-        try:
-            base_url = os.environ["BASE_URL"].rstrip("/")
-        except:
-            base_url = None
+        base_url = os.environ.get("BASE_URL", "").rstrip("/")
         if base_url:
             # Create a queue
             g_update_queue = asyncio.Queue()
@@ -39,14 +39,14 @@ def main():
 
             # Create a webserver
             app = web.Application()
-            app.router.add_route("GET", webhook_path, webhook)
-            app.router.add_route("POST", webhook_path, webhook)
+            app.router.add_route("GET", webhook_path, _webhook)
+            app.router.add_route("POST", webhook_path, _webhook)
 
             # Register the server with the event loop
             port = int(os.environ["PORT"]) if "PORT" in os.environ else 80
-            loop.run_until_complete(loop.create_server(app.make_handler(),
-                                                       "0.0.0.0",
-                                                       port))
+            loop.run_until_complete(
+                loop.create_server(app.make_handler(), "0.0.0.0", port)
+            )
 
             # Set the webhook URL with Telegram
             loop.run_until_complete(bot.setWebhook(base_url + webhook_path))
@@ -62,4 +62,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    _main()
